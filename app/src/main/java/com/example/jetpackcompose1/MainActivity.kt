41bee6e1acefbfbb909a32947e72a9c7663fb1a8
mohamed tamer0 <mt4452242@gmail.com>
2021-01-06 19:22:49 +0200

    update dependencies

--- app/src/main/java/com/example/jetpackcompose1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose1/MainActivity.kt
@@ -7,25 +7,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.jetpackcompose1.ui.Jetpackcompose1Theme
-import java.lang.reflect.Modifier
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Column(
-                    modifier = androidx.compose.ui.Modifier.padding(16.dp)
+                    modifier = Modifier.padding(16.dp)
             ) {
 
                 Text(text = "Hey Look Some Text")
-                Spacer(modifier = androidx.compose.ui.Modifier.padding(top = 10.dp))
+                Spacer(modifier = Modifier.padding(top = 10.dp))
                 Button(
                         onClick = { }
                 ) {
