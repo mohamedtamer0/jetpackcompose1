a1f31a7dc1bb6a7f5a79fa06d17037d1e88cd56b
mohamed tamer0 <mt4452242@gmail.com>
2021-01-08 15:21:11 +0200

    update

--- app/src/main/java/com/example/jetpackcompose1/RecipeListFragment.kt
+++ app/src/main/java/com/example/jetpackcompose1/RecipeListFragment.kt
@@ -4,8 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
 
 class RecipeListFragment : Fragment () {
@@ -16,11 +27,26 @@ class RecipeListFragment : Fragment () {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return ComposeView(requireContext()).apply {
-            setContent {
-                Text(text = "Recipe List Fragment")
+       val view = inflater.inflate(R.layout.fragment_recipe_list, container,
+       false)
+        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+            Column(modifier = Modifier
+                .border(border = BorderStroke(1.dp, Color.Black))
+                .padding(16.dp)
+            ) {
+                Text(text = "THIS IS A COMPOSE INSIDE T HE FRAGMENT")
+                Spacer(modifier = Modifier.padding(10.dp))
+                CircularProgressIndicator()
+                Spacer(modifier = Modifier.padding(10.dp))
+                Text(text = "NEAT")
+                Spacer(modifier = Modifier.padding(10.dp))
+
+                val customView = HorizontalDottedProgress(ContextAmbient.current)
+                AndroidView(viewBlock = {customView})
+
             }
         }
+        return view
     }
 
 
